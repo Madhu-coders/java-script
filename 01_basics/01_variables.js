1a1f75ff89b05c01fad42962f5fe92e2813951a2
Madhu Agrahari <agraharimadhu66@gmail.com>
2024-10-17 10:34:16 +0000

    declared

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1444553
+let accountEmail = "agraharimadhu66@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+//accountId = 2 // not allowed 
+
+console.log(accountId);
+
